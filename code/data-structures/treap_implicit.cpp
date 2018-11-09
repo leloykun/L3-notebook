985c8ba66c29a3d8705dc0c3f93116e9108b2d59
leloykun <franzlouiscesista@gmail.com>
2018-11-09 09:40:28 +0800

    debugged treap_implicit.cpp

--- code/data-structures/treap_implicit.cpp
+++ code/data-structures/treap_implicit.cpp
@@ -2,7 +2,7 @@ struct cartree {
   typedef struct _Node {
     int node_val, subtree_val, delta, prio, size;
     _Node *l, *r;
-    _Node(long long val) : node_val(val), subtree_val(val),
+    _Node(int val) : node_val(val), subtree_val(val),
         delta(0), prio((rand()<<16)^rand()), size(1),
         l(NULL), r(NULL) {}
     ~_Node() { delete l; delete r; }
@@ -11,52 +11,52 @@ struct cartree {
     return v ? v->subtree_val : 0;  }
   int get_size(Node v) { return v ? v->size : 0; }
   void apply_delta(Node v, int delta) {
-    if (!v) continue;
+    if (!v) return;
     v->delta += delta;
     v->node_val += delta;
-    v->subtree_val += delta * v->get_size();  }  }
+    v->subtree_val += delta * get_size(v);  }
   void push_delta(Node v) {
-    if (!v) continue;
-    v->l->apply_delta(v->delta);
-    v->r->apply_delta(v->delta);
+    if (!v) return;
+    apply_delta(v->l, v->delta);
+    apply_delta(v->r, v->delta);
     v->delta = 0;  }
   void update(Node v) {
-    if (!v) continue;
-    v->subtree_val = v->l->get_subtree_val() + v->node_val
-                   + v->r->get_subtree_val();
-    v->size = v->l->get_size() + 1 + v->r->get_size(); }
+    if (!v) return;
+    v->subtree_val = get_subtree_val(v->l) + v->node_val
+                   + get_subtree_val(v->r);
+    v->size = get_size(v->l) + 1 + get_size(v->r); }
   Node merge(Node l, Node r) {
-    l->push_delta();    r->push_delta();
+    push_delta(l);    push_delta(r);
     if (!l || !r)   return l ? l : r;
     if (l->size <= r->size) {
       l->r = merge(l->r, r);
-      l->update();
+      update(l);
       return l;
     } else {
       r->l = merge(l, r->l);
-      r->update();
+      update(r);
       return r;  }  }
   void split(Node v, int key, Node &l, Node &r) {
-    v->push_delta();
+    push_delta(v);
     l = r = NULL;
     if (!v)     return;
-    if (key <= v->l->get_size()) {
+    if (key <= get_size(v->l)) {
       split(v->l, key, l, v->l);
       r = v;
     } else {
-      split(v->r, key - v->l->get_size() - 1, v->r, r);
+      split(v->r, key - get_size(v->l) - 1, v->r, r);
       l = v;  }
-    v->update();  }
+    update(v);  }
   Node root;
 public:
   cartree() : root(NULL) {}
   ~cartree() { delete root; }
   int get(Node v, int key) {
-    v->push_delta();
-    if (key < v->l->get_size())
+    push_delta(v);
+    if (key < get_size(v->l))
       return get(v->l, key);
-    else if (key > v->l->get_size())
-      return get(v->r, key - v->l->get_size() - 1);
+    else if (key > get_size(v->l))
+      return get(v->r, key - get_size(v->l) - 1);
     return v->node_val;  }
   int get(int key) { return get(root, key); }
   void insert(Node item, int key) {
@@ -71,21 +71,21 @@ public:
     split(m, key, l, m);
     delete m;
     root = merge(l, r);  }
-  long long query(int a, int b) {
+  int query(int a, int b) {
     Node l1, r1;
     split(root, b+1, l1, r1);
     Node l2, r2;
     split(l1, a, l2, r2);
-    long long res = r2->get_subtree_val();
+    int res = get_subtree_val(r2);
     l1 = merge(l2, r2);
     root = merge(l1, r1);
     return res;  }
-  long long modify(int a, int b, ll delta) {
+  int update(int a, int b, int delta) {
     Node l1, r1;
     split(root, b+1, l1, r1);
     Node l2, r2;
     split(l1, a, l2, r2);
-    r2->apply_delta(delta);
-    merge(l2, r2);
-    merge(l1, r1);  }
-  int size() { return root->get_size(); }  };
+    apply_delta(r2, delta);
+    l1 = merge(l2, r2);
+    root = merge(l1, r1);  }
+  int size() { return get_size(root); }  };
