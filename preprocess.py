3037df03f272c448f7022acd3caac4048d5112ec
leloykun <franzlouiscesista@gmail.com>
2019-09-25 20:21:28 +0800

    added heavy_light_decomposition

--- preprocess.py
+++ preprocess.py
@@ -15,7 +15,8 @@ for path, dirs, files in os.walk('./code'):
     for f in files:
         if (f.endswith(".cpp") or f.endswith('.java') or f.endswith('.sh')) and not f.endswith(".test.cpp"):
             p = os.path.join(path, f)
-            q = os.path.join("_code", '/'.join(path.split('/')[2:]), f)
+            q = os.path.join("_code", '/'.join(path.split('\\')[1:]), f)
+            print(q)
 
             try:
                 os.makedirs(os.path.dirname(q))
