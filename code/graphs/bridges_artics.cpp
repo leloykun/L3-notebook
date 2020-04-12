c2363ae6bab016dc5cb7928a136c0c4a4c5d2ff1
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2020-04-13 05:08:17 +0800

    Apr 13 update

--- code/graphs/bridges_artics.cpp
+++ code/graphs/bridges_artics.cpp
@@ -1,8 +1,10 @@
 vii bridges;
-vi adj[MAXN], disc, low, articulation_points;
+vi adj[MAXN], disc(MAXN, -1), low(MAXN), articulation_points, st;
+vvi comps;
 int TIME;
 void bridges_artics (int u, int p) {
   disc[u] = low[u] = TIME++;
+  st.push_back(u);
   int children = 0;
   bool has_low_child = false;
   for (int v : adj[u]) {
@@ -11,11 +13,17 @@ void bridges_artics (int u, int p) {
       children++;
       if (disc[u] < low[v])
         bridges.push_back({u, v});
-      if (disc[u] <= low[v])
+      if (disc[u] <= low[v]) {
         has_low_child = true;
-      low[u] = min(low[u], low[v]);
+        comps.push_back({u});
+        while (comps.back().back() != v and !st.empty()) {
+          comps.back().push_back(st.back());
+          st.pop_back();
+        }
+      }
+      low[u] = std::min(low[u], low[v]);
     } else if (v != p)
-      low[u] = min(low[u], disc[v]);
+      low[u] = std::min(low[u], disc[v]);
   }
   if ((p == -1 && children >= 2) ||
       (p != -1 && has_low_child))
--- codelib/graph_theory/bridges_artics.cpp
+++ codelib/graph_theory/bridges_artics.cpp
@@ -1,59 +1,70 @@
 #include <iostream>
-#include <utility>
 #include <vector>
-using namespace std;
+#include <utility>
 
-const int MAX_N = 1000;
-vector<int> adj[MAX_N];
-vector<int> disc;
-vector<int> low;
+typedef std::pair<int, int> ii;
+typedef std::vector<ii> vii;
+typedef std::vector<int> vi;
+typedef std::vector<vi> vvi;
+
+const int MAXN = 1e5;
+
+vii bridges;
+vi adj[MAXN], disc(MAXN, -1), low(MAXN), articulation_points, st;
+vvi comps;
 int TIME;
-
-vector<pair<int, int> > bridges;
-vector<int> articulation_points;
-
+void add_edge(int u, int v) {
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+}
 void bridges_artics (int u, int p) {
   disc[u] = low[u] = TIME++;
+  st.push_back(u);
   int children = 0;
   bool has_low_child = false;
   for (int v : adj[u]) {
     if (disc[v] == -1) {
       bridges_artics(v, u);
       children++;
-
       if (disc[u] < low[v])
         bridges.push_back({u, v});
-      if (disc[u] <= low[v])
+      if (disc[u] <= low[v]) {
         has_low_child = true;
-
-      low[u] = min(low[u], low[v]);
+        
+        comps.push_back({u});
+        while (comps.back().back() != v and !st.empty()) {
+          comps.back().push_back(st.back());
+          st.pop_back();
+        }
+      }
+      low[u] = std::min(low[u], low[v]);
     } else if (v != p)
-      low[u] = min(low[u], disc[v]);
+      low[u] = std::min(low[u], disc[v]);
   }
-
-  if ((p == -1 && children >= 2) || (p != -1 && has_low_child))
+  if ((p == -1 && children >= 2) ||
+      (p != -1 && has_low_child))
     articulation_points.push_back(u);
 }
 
 int main() {
-  int N;  cin>>N;
-  disc.assign(N, -1);
-  low.assign(N, -1);
+  add_edge(0, 1);
+  add_edge(1, 2);
+  add_edge(2, 3);
+  add_edge(2, 0);
 
   TIME = 0;
-  for (int u = 0; u < N; ++u)
-    if (disc[u] == -1)
-      bridges_artics(u, -1);
+  bridges_artics(0, -1);
 
-  cout<<"articulation points:\n";
+  std::cout << "articulation_points:\n";
   for (int u : articulation_points)
-    cout<<u<<" ";
-  cout<<"\n";
+    std::cout << u << " ";
+  std::cout << "\n";
 
-  cout<<"bridges\n";
-  for (pair<int, int> bridge : bridges)
-    cout<<bridge.first<<" "<<bridge.second<<"\n";
-  cout<<"\n";
+  for (auto v : comps) {
+    for (int u : v)
+      std::cout << u << " ";
+    std::cout << "\n";
+  }
 
   return 0;
 }
