a95f6b4aedb553060d468e8ed8dd57d8a1c147fb
leloykun <franzlouiscesista@gmail.com>
2021-03-18 15:35:58 +0800

    added truncated notebook

--- makefile.sh
+++ makefile.sh
@@ -1,6 +1,11 @@
 rm -f *.{aux,log,toc,fls,fdb_latexmk}
 python3 preprocess.py
-pdflatex -shell-escape notebook.tex
-pdflatex -shell-escape notebook.tex
+
+pdflatex -shell-escape -jobname="notebook-trunc" notebook.tex
+pdflatex -shell-escape -jobname="notebook-trunc" notebook.tex
+
+pdflatex -shell-escape "\def\ICPCCONFIG{1} \input{notebook.tex}"
+pdflatex -shell-escape "\def\ICPCCONFIG{1} \input{notebook.tex}"
+
 rm -f *.{aux,log,toc,fls,fdb_latexmk}
 rm -rf _code/
